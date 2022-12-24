bc3b8e405c83789a7403c2bc571de83d23148bad
Sai Kumar <prasanna.kandregula.lpk@gmail.com>
2022-12-24 21:50:34 +0530

    Feat: Set game score to Fifteen-Love if player one scores one point.

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,16 +1,39 @@
-import React from "react";
-import { applicationConstants } from "../../constants/applicationConstants";
+import React, { useEffect, useState } from "react";
+import {
+  applicationConstants,
+  score,
+} from "../../constants/applicationConstants";
 import "./index.css";
 
-const { GAME_SCORE, LOVE_ALL, PLAYER_ONE, PLAYER_TWO, TITLE } =
+const { GAME_SCORE, PLAYER_ONE, PLAYER_TWO, TITLE, POINT_ONE } =
   applicationConstants;
+const { LOVE_ALL, FIFTEEN_LOVE } = score;
 
 const Game = () => {
+  const [gameScore, setGameScore] = useState(LOVE_ALL);
+  const [playerOneScore, setPlayerOneScore] = useState(0);
+
+  const incrementScore = () => {
+    setPlayerOneScore(playerOneScore + POINT_ONE);
+  };
+
+  const calculateGameScore = () => {
+    if (playerOneScore === POINT_ONE) {
+      setGameScore(FIFTEEN_LOVE);
+    }
+  };
+
+  useEffect(() => {
+    calculateGameScore();
+  }, [playerOneScore]);
+
   return (
     <React.Fragment>
       <h1>{TITLE}</h1>
-      <p data-testid={GAME_SCORE}>{LOVE_ALL}</p>
-      <button>{PLAYER_ONE}</button>
+      <p data-testid={GAME_SCORE}>{gameScore}</p>
+      <button data-testid={PLAYER_ONE} onClick={incrementScore}>
+        {PLAYER_ONE}
+      </button>
       <button>{PLAYER_TWO}</button>
     </React.Fragment>
   );
--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,7 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "../App";
 
 test("When the Game Starts, then the initial score should be Love-All", () => {
   render(<App />);
   expect(screen.getByTestId("game_score").textContent).toEqual("Love-All");
+});
+
+test("When the running point of player one is 1 then the running score should be Fifteen-Love", () => {
+  render(<App />);
+  fireEvent.click(screen.getByTestId("Player One"));
+
+  expect(screen.getByTestId("game_score").textContent).toEqual("Fifteen-Love");
 });
