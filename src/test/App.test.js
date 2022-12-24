f1bc393802606029513275e036e825fa39a34ba0
Sai Kumar <prasanna.kandregula.lpk@gmail.com>
2022-12-24 22:01:03 +0530

    Refractor: Moving the redundant test code into the generic functions.

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,14 +1,23 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "../App";
+import { testConstants } from "./constants/testConstants";
+
+const { GAME_SCORE, LOVE_ALL, FIFTEEN_LOVE, PLAYER_ONE } = testConstants;
+
+beforeEach(() => {
+  render(<App />);
+});
+
+const gameScoreShouldBe = (expected) => {
+  expect(screen.getByTestId(GAME_SCORE).textContent).toEqual(expected);
+};
 
 test("When the Game Starts, then the initial score should be Love-All", () => {
-  render(<App />);
-  expect(screen.getByTestId("game_score").textContent).toEqual("Love-All");
+  gameScoreShouldBe(LOVE_ALL);
 });
 
 test("When the running point of player one is 1 then the running score should be Fifteen-Love", () => {
-  render(<App />);
-  fireEvent.click(screen.getByTestId("Player One"));
+  fireEvent.click(screen.getByTestId(PLAYER_ONE));
 
-  expect(screen.getByTestId("game_score").textContent).toEqual("Fifteen-Love");
+  gameScoreShouldBe(FIFTEEN_LOVE);
 });
---
+++ src/test/constants/testConstants.js
@@ -0,0 +1,8 @@
+const testConstants = {
+  GAME_SCORE: "game_score",
+  LOVE_ALL: "Love-All",
+  FIFTEEN_LOVE: "Fifteen-Love",
+  PLAYER_ONE: "Player One",
+};
+
+export { testConstants };
